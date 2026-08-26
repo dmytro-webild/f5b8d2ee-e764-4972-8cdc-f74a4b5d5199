9fb461f62c874917774d5f7a4cbd2787d3d27560
bender <kudin.dima@gmail.com>
2026-08-26 08:21:49 +0000

    Switch to version 8: modified src/components/Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -12,7 +12,7 @@ export default function Layout() {
       "href": "#hero"
     },
     {
-      "name": "Automatización IA",
+      "name": "IA Automation",
       "href": "#features"
     },
     {
