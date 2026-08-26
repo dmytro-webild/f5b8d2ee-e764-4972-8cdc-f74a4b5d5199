a7c5b32ee182b8cf997d3f6afb0125a394f2b53c
bender <kudin.dima@gmail.com>
2026-08-26 08:21:50 +0000

    Switch to version 8: modified src/pages/HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,7 +6,7 @@
 
 import React from 'react';
 import HeroSection from './HomePage/sections/Hero';
-import NosotrosSection from './HomePage/sections/Nosotros';
+import AboutSection from './HomePage/sections/About';
 import FeaturesSection from './HomePage/sections/Features';
 import BentoSection from './HomePage/sections/Bento';
 import MetricsSection from './HomePage/sections/Metrics';
@@ -18,7 +18,7 @@ export default function HomePage(): React.JSX.Element {
     <>
       <HeroSection />
 
-      <NosotrosSection />
+      <AboutSection />
 
       <FeaturesSection />
 
